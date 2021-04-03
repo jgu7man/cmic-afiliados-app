ca7afeb898fbc00f762b52ef6246e2395cd8d289
CesarsEren <cpinedo428@gmail.com>
2021-04-02 22:22:41 -0500

    -Vista 1 Formulario de Afiliados -Dialog de Privacidad -Dialog de Retención

--- src/app/public/afiliados/afiliados.module.ts
+++ src/app/public/afiliados/afiliados.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AfiliadosRoutingModule } from './afiliados-routing.module';
 import { AfiliadosComponent } from './afiliados.component';
-import { AfiliacionFormComponent } from './components/afiliacion-form/afiliacion-form.component';
+import { AfiliacionFormComponent, DialogPrivacidad, DialogRetencion } from './components/afiliacion-form/afiliacion-form.component';
 import { MaterialModule } from 'src/shared/material.module';
 import { FirebaseModule } from 'src/shared/firebase.module';
 import { ComunesModule } from 'src/shared/comunes.module';
 
 
 @NgModule({
-  declarations: [AfiliadosComponent, AfiliacionFormComponent],
+  declarations: [AfiliadosComponent, AfiliacionFormComponent, DialogPrivacidad, DialogRetencion],
   imports: [
     CommonModule,
     AfiliadosRoutingModule,
--- src/app/public/afiliados/components/afiliacion-form/afiliacion-form.component.ts
+++ src/app/public/afiliados/components/afiliacion-form/afiliacion-form.component.ts
@@ -1,24 +1,100 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { DatosGenerales } from '../../afiliados.model';
 
 @Component({
   selector: 'g-afiliados-form',
   templateUrl: './afiliacion-form.component.html',
-  styleUrls: ['./afiliacion-form.component.scss']
+  styleUrls: ['./afiliacion-form.component.scss'],
 })
 export class AfiliacionFormComponent implements OnInit {
-
   // REVIEW La forma de cargarlos los datos con clase hace más simple su iniciación
-  public datosGenerales: DatosGenerales
+  public datosGenerales: DatosGenerales;
 
-  constructor() {
-    this.datosGenerales = new DatosGenerales('','','','','','','','');
+  constructor(public dialog: MatDialog) {
+    this.datosGenerales = new DatosGenerales(
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      false,
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      false,
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      '',  
+      
+      false,
+      false,
+      false
+    );
   }
 
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  OpenPrivacidad(): void {
+    const dialogRef = this.dialog.open(DialogPrivacidad);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
+  OpenRetencion(): void {
+    const dialogRef = this.dialog.open(DialogRetencion);
 
-
-
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+
+@Component({
+  selector: 'dialog-privacidad',
+  templateUrl: 'dialog-privacidad.html',
+})
+export class DialogPrivacidad {}
+
+@Component({
+  selector: 'dialog-retencion',
+  templateUrl: 'dialog-retencion.html',
+})
+export class DialogRetencion {}
