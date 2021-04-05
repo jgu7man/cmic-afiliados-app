fd46f102e6c8760aa20dacef0d66983f890057f4
jgu7man <jgu7man@gmail.com>
2021-04-04 23:43:18 -0500

    restructuracion de fromularios

--- src/app/public/afiliados/components/afiliacion-form/afiliacion-form.component.ts
+++ src/app/public/afiliados/components/afiliacion-form/afiliacion-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { DatosGenerales } from '../../afiliados.model';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { AfiliadoModel, AutorizacionesModel, ContactoModel, CorrespondenciaModel, DatosGeneralesModel, DirectorModel, RepresentanteLegalModel } from '../../afiliados.model';
 
 @Component({
   selector: 'g-afiliados-form',
@@ -9,64 +9,30 @@ import { DatosGenerales } from '../../afiliados.model';
 })
 export class AfiliacionFormComponent implements OnInit {
   // REVIEW La forma de cargarlos los datos con clase hace más simple su iniciación
-  public datosGenerales: DatosGenerales;
+  public afiliado: AfiliadoModel
+  public datosGenerales: DatosGeneralesModel
+  public correspondencia: CorrespondenciaModel
+  public contacto: ContactoModel
+  public representanteLegal: RepresentanteLegalModel
+  public director: DirectorModel
+  public autorizaciones: AutorizacionesModel
 
   constructor(public dialog: MatDialog) {
-    this.datosGenerales = new DatosGenerales(
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      false,
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      false,
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      '',  
-      
-      false,
-      false,
-      false
-    );
+
+    this.datosGenerales = new DatosGeneralesModel('', '', '', '', '', '', '', '', '', '', '', '')
+    this.correspondencia = new CorrespondenciaModel(false, '', '', '', '', '','','')
+    this.contacto = new ContactoModel('', '', '', '', '', '')
+    this.representanteLegal = new RepresentanteLegalModel('', '', '', '', '', '', '', '', '', '', '', false)
+    this.director = new DirectorModel('', '', '', '', '', '', '', '', '', '', '')
+    this.autorizaciones = new AutorizacionesModel(false, false, false)
+    this.afiliado = new AfiliadoModel(
+      this.datosGenerales,
+      this.correspondencia,
+      this.contacto,
+      this.representanteLegal,
+      this.director,
+      this.autorizaciones,
+    )
   }
 
   ngOnInit(): void {}
@@ -76,6 +42,7 @@ export class AfiliacionFormComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
+      // TODO Capturar el evento de aceptar en el model y validar el formulario
     });
   }
   OpenRetencion(): void {
@@ -91,7 +58,12 @@ export class AfiliacionFormComponent implements OnInit {
   selector: 'dialog-privacidad',
   templateUrl: 'dialog-privacidad.html',
 })
-export class DialogPrivacidad {}
+export class DialogPrivacidad {
+  // REVIEW agregar las importaciones para el manejo de la data en el DIALOG
+  constructor(
+    public dialog_: MatDialogRef<DialogPrivacidad>
+  ){}
+}
 
 @Component({
   selector: 'dialog-retencion',
